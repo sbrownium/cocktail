00bd1e454354d7cb778415208bfaa1a23ca3d94b
sbrownium <brownscottbrown@gmail.com>
2023-11-01 17:01:34 -0700

    Initial Requirements

--- src/App.js
+++ src/App.js
@@ -23,3 +23,28 @@ function App() {
 }
 
 export default App;
+
+/* 
+Login page
+  • Username
+  • password
+  • Attempt to only make field for sign-up/login. Queries the database for username and
+  if you can't find it then takes to new sign-up step
+
+Restaurant page
+  • Restaurant name
+  • Drink list
+    – Name
+    - Description
+  • Comments
+    - Add new
+    - Expand/read
+
+Database
+  • Needs to contain:
+    - username
+    - password
+    - comments
+      • date
+      • thumbs up, down, the best emoji, and puke face
+*/
