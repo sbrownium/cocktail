5321b9de327612ef285e7822d32d0ff498b7132b
sbrownium <brownscottbrown@gmail.com>
2024-01-18 17:13:45 -0800

    Comment

--- src/Drink.js
+++ src/Drink.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Comment from './Comment';
 
 export default function Drink({drinksAtBar}){
     return (
         <ul>
       {drinksAtBar.map(({name, description, price}, index) => (
           <li key={index}>
-            {name} &mdash; &nbsp;
-            {description} &mdash; &nbsp;
+            {name} &mdash;&nbsp;
+            {description} &mdash;
             ${price.toFixed(2)}
+            <Comment/>
           </li>
              ))}
            </ul> 
