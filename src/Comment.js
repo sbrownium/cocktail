f2b24165c6f78fad801725a00f16cf6a8846ca34
sbrownium <118406952+sbrownium@users.noreply.github.com>
2024-03-09 09:32:50 -0800

    consoliate Comment state and toggle functionality for Never Mind button

--- src/Comment.js
+++ src/Comment.js
@@ -6,20 +6,24 @@ import { UserContext } from './UserContext.js';
 
 export default function Comment ({index, commentID, text, userName, date, userID}) {
     const [user, setUser] = useContext(UserContext);
-    const [showEdit, setShowEdit] = useState(false);
     const [ beingEditted, setBeingEditted ] = useState(false)
  
+    function handleToggle() {
+        setBeingEditted(beingEditted => !beingEditted); 
+    }
+
     function handleEdit(e){
         e.preventDefault();
-        setShowEdit(showEdit => !showEdit);
-        setBeingEditted(beingEditted => !beingEditted); 
+        handleToggle(); 
     };
 return (
         <li key={index} id={commentID}>
-            {showEdit ? <EditBox text={text}/> : text }
+            {beingEditted ? <EditBox text={text}/> : text }
              &nbsp;
             {beingEditted ? '' : <> ({userName} &mdash; {date})</>}
-            {user.userID === userID ? <EditIcon beingEditted={beingEditted} handleEdit={handleEdit}/> : '' }
+            {user.userID === userID ? <EditIcon beingEditted={beingEditted} handleToggle={handleToggle} handleEdit={handleEdit}/> : '' }
+            {/* {beingEditted ? '' : <> ({userName} &mdash; {date})</>}
+            {user.userID === userID ? <EditIcon beingEditted={beingEditted} handleEdit={handleEdit}/> : '' } */}
         </li>
 )
 };
--- src/EditIcon.js
+++ src/EditIcon.js
@@ -2,13 +2,13 @@ import React from "react";
 import './EditIcon.css';
 import PencilIcon from "./PencilIcon";
 
-export default function Edit ({handleEdit, beingEditted}){
+export default function EditIcon ({handleEdit, handleToggle, beingEditted}){
     return (
         < >
         {beingEditted ? 
         <>
         <button onClick={handleEdit}>Save</button> 
-        <button>Never Mind</button>
+        <button onClick={handleToggle}>Never Mind</button>
         </> : 
         (<button className='edit' onClick={handleEdit}>
             <PencilIcon fillColor='black'/>
