ab8f2c4769cc00de3bca714bf900959a5d85a67e
sbrownium <118406952+sbrownium@users.noreply.github.com>
2024-03-11 20:24:21 -0700

    NewRating writing to database

--- src/NewRating.js
+++ src/NewRating.js
@@ -7,75 +7,62 @@ import Submit from './Submit.js';
 
 export default function NewRating({emojiLookUp, ratingDrinkID}) {
 
-//     const [rating, setRating] = useState('');
-//     const [user, setUser] = useContext(UserContext);
-//     const { userName, userID } = user 
+    const [rating, setRating] = useState('');
+    const [user, setUser] = useContext(UserContext);
+    const { userName, userID } = user 
 
-//     function handleClick(e) {
-//       e.preventDefault();
-//       if (!user) { 
-//         return (
-//           alert('Please login to add a comment')
-//         )}
-//         else {
-//       const newCommentKey = push(child(ref(db), '/comments/')).key;
-//       const newVersionKey = push(child(ref(db), '/commentVersions/')).key;
-//       const updates = {};
-//       const newComment = {
-//         commentID: newCommentKey,
-//         userName: userName,
-//         userID: userID,
-//         drinkID: commentDrinkID,
-//         timeStamp: performance.timeOrigin,
-//         text: comment,
-//       };
-//       const newVersion = {
-//         commentID: newCommentKey,
-//         versionID: newVersionKey,
-//         timeStamp: performance.timeOrigin,
-//         text: comment
-//       }
-//     setComment('');
-//     updates['/comments/' + newCommentKey] = newComment;
-//     updates['/commentVersions/' + newVersionKey] = newVersion;
+    function handleRating (e) {
+        setRating({
+            rating: e.target.value
+        })
+}
+
+    function handleClick(e) {
+      e.preventDefault();
+      if (!user) { 
+        return (
+          alert('Please login to rate a drink')
+        )}
+        // if user has already rated alert that will change rating (return what was already rated)
+        else {
+      const newRatingKey = push(child(ref(db), '/ratings/')).key;
+      const updates = {};
+      const newRating = {
+        ratingID: newRatingKey,
+        userName: userName,
+        userID: userID,
+        drinkID: ratingDrinkID,
+        timeStamp: performance.timeOrigin,
+        rating: Number(rating.rating)
+      };
+    setRating('');
+    updates['/ratings/' + newRatingKey] = newRating;
    
-//     return (
-//         update(ref(db), updates).then(() => {
-//             console.log('Data saved successfully!')
-//       })
-//       .catch((error) => {
-//         console.log('problem writing')
-//       })
-//     ) }
-// }
-// 
-
     return (
-        
+        update(ref(db), updates).then(() => {
+            console.log('Data saved successfully!')
+      })
+      .catch((error) => {
+        console.log('problem writing')
+      })
+    ) }
+}
+    return (  
       <>
         <form>
           <label>Rating
-          <select name='ratingSelect'>
-            <option>Please Select a Rating:</option>
+          <select name='ratingSelect'
+          value={rating.rating} 
+          onChange={handleRating}
+          >
+            <option>Rate</option>
             {Object.entries(emojiLookUp).map(([key, value], index) => (
     <option key={index} value={value}>{key}</option>
 ))}
-
           </select>
           </label>
-          {/* <Submit handleClick={handleClick} value='add'/> */}
+          <Submit handleClick={handleClick} value='add'/>
         </form>
       </>
     );
   }
-
-
-//   <option value='Please choose a bar'>Please choose a bar</option> 
-//             {barsArray.map(({ barName, barID }, index) => {
-//               if (!uniqueBars.has(barID)) {
-//                 uniqueBars.add(barID);
-//                 return (
-//                   <option key={index} value={barID}>{barName}</option>
-//                 )
-//               }})}
-//             <option isNew={true} className='new' value='new'>New</option>
