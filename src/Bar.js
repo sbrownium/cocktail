fcee919873650b3ccd5337154ad713e067fb3ea2
sbrownium <118406952+sbrownium@users.noreply.github.com>
2024-03-31 12:24:56 -0700

    Bars rerendering onChange

--- src/Bar.js
+++ src/Bar.js
@@ -1,29 +1,36 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import { UserContext } from "./UserContext";
 import Drink from './Drink.js'
 import ChangeBar from './ChangeBar.js';
-import Submit from './Submit.js';
+
 
 export default function Bar({bars, drinks, comments, ratings}) {
   const [user, setUser] = useContext(UserContext);
+  const initialSelectedBar = '-Nr7lOhIhP11igBsjqOp';
+  const [selectedBar, setSelectedBar] = useState(initialSelectedBar);
   const barsArray = Object.values(bars);
+  const filteredBars = barsArray.filter(bar => bar.barID === selectedBar);
 
   function handleSelect (e) {
     e.preventDefault();
+    if (!user) {
+      setSelectedBar(
+        e.target.value
+    );      
+    } else {
             setUser({
                 ...user,
                 lastBar: e.target.value
-            });   
-        } 
-
-  function handleClick () {
-    console.log('click handled')
-  }
+            });
+            setSelectedBar(
+              e.target.value
+          );      
+        } }
   
     return (
       <>
       <ul>
-        {barsArray.map(({ barName, barID }, index) => {
+        {filteredBars.map(({ barName, barID }, index) => {
             return (
               <li key={index}>
                  <h1>{barName}</h1>
@@ -34,7 +41,6 @@ export default function Bar({bars, drinks, comments, ratings}) {
       </ul>
       <form>
       <ChangeBar bars={bars} handleSelect={handleSelect}/> 
-      <Submit handleClick={handleClick} value='change'/>
       </form>
       </>
     );
