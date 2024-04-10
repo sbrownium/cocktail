bb2cd25211e9a0aa0cdddc3dfcd76a0985b7cf76
sbrownium <118406952+sbrownium@users.noreply.github.com>
2024-04-10 15:22:23 -0700

    BarSelector defaultValue from Bar

--- src/BarSelector.js
+++ src/BarSelector.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './BarSelector.css';
 
-export default function BarSelector({bars, newDrink, validation, handleExistingBar, focusSelector}) {
+export default function BarSelector({bars, newDrink, validation, handleExistingBar, focusSelector, defaultBar}) {
     const barsArray = Object.values(bars);
     const uniqueBars = new Set();
       return (
         <>
           <label>Bar
-          <select name='barSelect' 
+          <select name='barSelect'
+          defaultValue={defaultBar} 
           className={validation.needsBarID ? 'missing' : null }
           value={newDrink.barID} 
           onChange={handleExistingBar}
           onFocus={focusSelector}>
-            <option value='Please choose a bar'>Please choose a bar</option> 
+            {/* <option value='Please choose a bar'>Please choose a bar</option>  */}
             {barsArray.map(({ barName, barID }, index) => {
               if (!uniqueBars.has(barID)) {
                 uniqueBars.add(barID);
