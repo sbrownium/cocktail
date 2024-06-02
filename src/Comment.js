5f0b567aeaf7ccab40ed4b18e5a0c66d4b86afc5
sbrownium <118406952+sbrownium@users.noreply.github.com>
2024-06-02 15:55:19 -0700

    comments only editable by comment owner

--- src/Comment.js
+++ src/Comment.js
@@ -1,13 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { ref, update } from "firebase/database";
 import { db } from "./firebase.js";
 import EditBox from './EditBox';
 import DeleteButton from './DeleteButton.js';
 import Time from './Time.js';
 import Button from './Button.js';
+import { UserContext } from './UserContext.js';
 
 
-export default function Comment ({ commentDrinkID, index, commentID, initialTimeStamp, text, users, userID, handleToggle, beingEditted}) {
+export default function Comment ({
+  commentDrinkID,
+  index,
+  commentID,
+  initialTimeStamp,
+  text,
+  users,
+  userID,
+  handleToggle,
+  beingEditted
+}) {
+    const [user] = useContext(UserContext);
     const [edit, setEdit] = useState(text);
     const usersArray = Object.values(users);
     const filteredUsers = usersArray.filter(u => u.userID === userID);
@@ -46,7 +58,7 @@ export default function Comment ({ commentDrinkID, index, commentID, initialTime
 return (
   
         <li key={index} id={commentID}>
-            {beingEditted ? 
+            {(beingEditted && userID === user.userID) ? 
             <>
                 <form>
                   <EditBox
